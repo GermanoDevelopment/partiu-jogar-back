cbf1cda39966d35ae349271d23d7eb8b03536c0c
Germano <germano-software-dev@hotmail.com>
2023-02-01 11:21:31 -0300

    fix: Abstract User entity to Profile entity.

--- src/modules/supervisor/entities/supervisor.entity.ts
+++ src/modules/supervisor/entities/supervisor.entity.ts
@@ -1,10 +1,14 @@
-import { Entity, ManyToOne, OneToMany } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
 import { School } from '../../../modules/school/entities/school.entity';
-import { User } from '../../../modules/user/entities/user.entity';
 import { Booktime } from '../../../modules/booktime/entities/booktime.entity';
+import { Profile } from '../../../modules/user/entities/profile.entity';
+import { ERole } from '../../../constants/role.enum';
 
 @Entity()
-export class Supervisor extends User {
+export class Supervisor extends Profile {
+    @Column({ nullable: false, default: ERole.SUPERVISOR, enum: ERole, type: 'enum' })
+    role: ERole;
+
     // relations
     @ManyToOne(() => School, (school) => school.supervisors)
     school: School;
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,28 +1,10 @@
-import { AbstractEntity } from '../../../common/abstract.entity';
 import { Column, Entity, OneToMany } from 'typeorm';
 import { ERole } from '../../../constants/role.enum';
 import { Booktime } from '../../../modules/booktime/entities/booktime.entity';
+import { Profile } from './profile.entity';
 
 @Entity()
-export class User extends AbstractEntity {
-    @Column({ nullable: true })
-    firstname: string;
-    
-    @Column({ nullable: true })
-    lastname: string;
-    
-    @Column({ unique: true, nullable: false })
-    email: string;
-
-    @Column({ unique: true, nullable: false })
-    cpf: string;
-
-    @Column({ nullable: true })
-    password: string;
-
-    @Column({ nullable: false, default: false })
-    confirmed: boolean;
-
+export class User extends Profile {
     @Column({ nullable: false, default: ERole.USER, enum: ERole, type: 'enum' })
     role: ERole;
     
