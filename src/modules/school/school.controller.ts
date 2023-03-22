794d76fc60680e964c3e7ce7bca5f97ee675c3f0
Germano <germano-software-dev@hotmail.com>
2023-03-22 16:32:36 -0300

    feat(School): Add dto returns.

--- src/modules/school/school.controller.ts
+++ src/modules/school/school.controller.ts
@@ -3,7 +3,7 @@ import { SchoolService } from './school.service';
 import { CreateSchoolDto } from './dto/CreateSchoolDto';
 import { UpdateSchoolDto } from './dto/UpdateSchoolDto';
 import { ApiTags } from '@nestjs/swagger';
-import { School } from './entities/school.entity';
+import { SchoolDto } from './dto/SchoolDto';
 
 @ApiTags('School')
 @Controller('school')
@@ -11,27 +11,27 @@ export class SchoolController {
   constructor(private readonly schoolService: SchoolService) {}
 
   @Post('create-school')
-  async create(@Body() createSchoolDto: CreateSchoolDto): Promise<School> {
+  async create(@Body() createSchoolDto: CreateSchoolDto): Promise<SchoolDto> {
     return await this.schoolService.create(createSchoolDto);
   }
 
   @Get('get-all-schools')
-  async findAll(): Promise<School[]> {
+  async findAll(): Promise<SchoolDto[]> {
     return await this.schoolService.findAll();
   }
 
   @Get('get-school/:id')
-  async findOne(@Param('id') id: string): Promise<School> {
+  async findOne(@Param('id') id: string): Promise<SchoolDto> {
     return await this.schoolService.findOne(id);
   }
 
   @Patch('update-school/:id')
-  async update(@Param('id') id: string, @Body() updateSchoolDto: UpdateSchoolDto): Promise<School> {
+  async update(@Param('id') id: string, @Body() updateSchoolDto: UpdateSchoolDto): Promise<SchoolDto> {
     return await this.schoolService.update(id, updateSchoolDto);
   }
 
   @Delete('delete-school/:id')
-  async remove(@Param('id') id: string): Promise<School> {
+  async remove(@Param('id') id: string): Promise<SchoolDto> {
     return await this.schoolService.remove(id);
   }
 }
