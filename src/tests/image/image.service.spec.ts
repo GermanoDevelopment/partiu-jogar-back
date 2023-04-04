57a8afe9084d9e05532717cb88fe66d0ad61f385
Germano <germano-software-dev@hotmail.com>
2023-04-04 15:34:56 -0300

    feat: Tests challenge.

---
+++ src/tests/image/image.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ImageService } from '../../modules/image/image.service';
+
+import { UploadModule } from '../../modules/upload/upload.module';
+import { Image } from '../../modules/image/entities/image.entity';
+
+describe('ImageService', () => {
+  let service: ImageService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ UploadModule ],
+      providers: [
+        ImageService,
+        {
+          provide: getRepositoryToken(Image),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<ImageService>(ImageService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
--- src/tests/school/school.service.spec.ts
+++ src/tests/school/school.service.spec.ts
@@ -1,91 +1,59 @@
 import { TestingModule, Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { ImageService } from '../../modules/image/image.service';
-import { UploadService } from '../../modules/upload/upload.service';
-import { Image } from '../../modules/image/entities/image.entity';
 import { Repository } from 'typeorm';
-import { mockImageRepository } from './mockImage.repository';
-import { CreateImageDto } from '../../modules/image/dto/CreateImageDto';
-import { ImageDto } from '../../modules/image/dto/ImageDto';
-import fs from 'fs';
-import { School } from 'src/modules/school/entities/school.entity';
-import { Court } from 'src/modules/court/entities/court.entity';
-import { InjectionToken } from '@nestjs/common';
 
-describe('ImageService', () => {
-  let imageService: ImageService;
-  let uploadService: UploadService;
+import { ImageModule } from '../../modules/image/image.module';
+import { ImageService } from '../../modules/image/image.service';
+import { SchoolService } from '../../modules/school/school.service';
+
+import { School } from '../../modules/school/entities/school.entity';
+import { CreateSchoolDto } from '../../modules/school/dto/CreateSchoolDto';
+import { Image } from '../../modules/image/entities/image.entity';
+import { UploadModule } from '../../modules/upload/upload.module';
+import { UploadService } from '../../modules/upload/upload.service';
+
+describe('SchoolService', () => {
   let moduleRef: TestingModule;
-  let imageRepository: Repository<Image>;
-  let uploadServiceToken: InjectionToken;
-
-  class UploadServiceMock extends UploadService {
-    async uploadFile(file): Promise<string> {
-      return 'path/to/image.jpg';
-    }
-  }
-
+  let service: SchoolService;
+  let repo: Repository<School>;
+  
   beforeEach(async () => {
     moduleRef = await Test.createTestingModule({
+      imports: [ ImageModule ],
       providers: [
-        ImageService,
+        SchoolService,
         {
-          provide: uploadServiceToken,
-          useClass: UploadServiceMock
-        },
-        {
-          provide: getRepositoryToken(Image),
-          useValue: mockImageRepository,
+          provide: getRepositoryToken(School),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+          },
         },
       ],
     }).compile();
 
-    imageService = moduleRef.get<ImageService>(ImageService);
-    uploadService = moduleRef.get<UploadService>(UploadService);
-    imageRepository = moduleRef.get<Repository<Image>>(
-      getRepositoryToken(Image),
-    );
+    service = moduleRef.get<SchoolService>(SchoolService);
+    repo = moduleRef.get<Repository<School>>(getRepositoryToken(School));
   });
 
-  afterEach(async () => {
-    await moduleRef.close();
-  });
-
-  describe('create', () => {
-    it('should create a new image', async () => {
-      const file = {
-        fieldname: 'file',
-        originalname: 'test.jpg',
-        encoding: '7bit',
-        mimetype: 'image/jpeg',
-        size: 7168,
-        buffer: Buffer.from('test'),
-        stream: fs.createReadStream('path/to/image.jpg'),
-        destination: 'path/to/destination',
-        filename: 'image.jpg',
-        path: 'path/to/destination/image.jpg'
-      };
-      jest.spyOn(uploadService, 'uploadFile').mockResolvedValue('path/to/image.jpg');
-      const createImageDto: CreateImageDto = {
-        schoolId: '123',
-        courtId: '456',
-      };
-
-      const result: ImageDto = await imageService.create(createImageDto, file);
-      const image = new Image();
-      image.id = 'test-id';
-      image.school = new School();
-      image.court = new Court();
-      image.path = 'path/to/image.jpg';
-
-      expect(uploadService.uploadFile).toHaveBeenCalledWith(file);
-      expect(imageRepository.create).toHaveBeenCalledWith({
-        schoolId: createImageDto.schoolId,
-        courtId: createImageDto.courtId,
-        path: 'path/to/image.jpg',
-      });
-      expect(imageRepository.save).toHaveBeenCalled();
-      expect(result).toEqual(new ImageDto(image));
+  describe('Service', () => {
+    it('should be defined', ()=> {
+      expect(service).toBeDefined();
+      expect(repo).toBeDefined();
     });
   });
+
+  // describe('CRUD', () => {
+  //   it('should create a new school', async () => {
+  //     const create: CreateSchoolDto = {
+  //       name: 'string',
+  //       location: 'string',
+  //       address: 'string',
+  //     };
+  //     const result = await service.create(create, { main: null, photos: null });
+  //     expect(result).toBeDefined();
+  //     expect(repo.create).toBeCalledTimes(1);
+  //     expect(repo.save).toBeCalledTimes(1);
+  //   });
+  // });
 });
