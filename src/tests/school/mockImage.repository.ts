2a350710eaa8bc006e055b6c811722e89e0309c1
Wyvig Israel <wyvigk5@gmail.com>
2023-04-10 10:32:07 -0300

    Test(School): o arquivo foi apagado e incorporado ao School.service.spec.ts

--- src/tests/school/mockImage.repository.ts
+++
@@ -1,8 +0,0 @@
-export const mockImageRepository = {
-    find: jest.fn(),
-    findBy: jest.fn(),
-    create: jest.fn(),
-    save: jest.fn(),
-    findOne: jest.fn(),
-    delete: jest.fn(),
-  };
